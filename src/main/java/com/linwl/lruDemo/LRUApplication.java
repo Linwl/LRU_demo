1fa3b94892afc71f95d7cca4b482ee1969f968e7
林万龙 <304115325@qq.com>
2019-11-07 14:50:56 +0800

    初始化项目

---
+++ src/main/java/com/linwl/lruDemo/LRUApplication.java
@@ -0,0 +1,15 @@
+package com.linwl.lruDemo;
+
+/**
+ * @author ：linwl
+ * @date ：Created in 2019/11/7 14:50
+ * @description ：
+ * @modified By：
+ */
+public class LRUApplication {
+
+    public static void main(String[] args)
+    {
+        
+    }
+}
